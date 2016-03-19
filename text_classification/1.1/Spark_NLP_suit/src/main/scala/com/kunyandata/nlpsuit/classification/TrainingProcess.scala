f1c6f7bf5e61dd79aca23806807cf2cdf797ad61
QQ <ricklovelisa@126.com>
2016-03-19 22:23:43 +0800

    update code

--- text_classification/1.1/Spark_NLP_suit/src/main/scala/com/kunyandata/nlpsuit/classification/TrainingProcess.scala
+++ text_classification/1.1/Spark_NLP_suit/src/main/scala/com/kunyandata/nlpsuit/classification/TrainingProcess.scala
@@ -5,6 +5,8 @@ package com.kunyandata.nlpsuit.classification
   */
 
 import java.io.{ObjectInputStream, FileInputStream, FileOutputStream, ObjectOutputStream}
+import org.apache.spark.ml.Pipeline
+import org.apache.spark.ml.feature._
 import org.apache.spark.ml.tuning.CrossValidator
 import org.apache.spark.mllib.feature
 import org.apache.spark.rdd.RDD
@@ -17,11 +19,13 @@ import org.apache.spark.sql.SQLContext
 import org.apache.spark.mllib.classification.SVMWithSGD
 import org.apache.spark.mllib.linalg.{Vectors, SparseVector}
 import com.kunyandata.nlpsuit.util.WordSeg
+import org.apache.spark.sql._
+import org.apache.spark.sql.types._
 
 
 private object TrainingProcess extends App{
 
-  def trainingProcessWithRDD(train: RDD[Seq[Object]], test :RDD[Seq[Object]]) = {
+  def trainingProcessWithRDD(train: RDD[Seq[Object]], test: RDD[Seq[Object]]) = {
     // 构建hashingTF模型，同时将数据转化为LabeledPoint类型
     val hashingTFModel = new feature.HashingTF()
     val trainTFRDD = train.map(line => {
@@ -92,8 +96,123 @@ private object TrainingProcess extends App{
     }
   }
 
+  def trainingProcessWithDF(sc: SparkContext, train:RDD[Seq[Object]], test: RDD[Seq[Object]]) = {
+    val sqlContext = new SQLContext(sc)
+    val schema =
+      StructType(
+        StructField("id", StringType, nullable = false) ::
+          StructField("category", StringType, nullable = false) ::
+          StructField("content", ArrayType(StringType, containsNull = true), nullable = false) ::
+          StructField("label", DoubleType, nullable = false) :: Nil)
+    val dataDF = sqlContext.createDataFrame(train.map(line => {
+      Row(line(0), line(1), line(2).asInstanceOf[Seq[String]].toArray, if(line(1) == "881155") 1.0 else 0.0)
+    }), schema).toDF()
+
+    val testDF = sqlContext.createDataFrame(test.map(line => {
+      Row(line(0), line(1), line(2).asInstanceOf[Seq[String]].toArray, if(line(1) == "881155") 1.0 else 0.0)
+    }), schema).toDF()
+
+    // token
+    val tokenizer = new Tokenizer()
+      .setInputCol("content")
+      .setOutputCol("words")
+    // 去除停用词
+    val stopWordsRemover = new StopWordsRemover()
+      .setStopWords(stopWords)
+      .setInputCol("content")
+      .setOutputCol("filtered")
+    //  val worddf = stopWordsRemover.transform(wordDataFrame)
+
+    // 构建向量空间模型
+//    val hashingTFModel = new HashingTF()
+//      .setInputCol(stopWordsRemover.getOutputCol)
+//      .setOutputCol("rawFeatures")
+//      .setNumFeatures(50000)
+
+    val cvModel = new CountVectorizer()
+      .setInputCol(stopWordsRemover.getOutputCol)
+      .setOutputCol("rawFeatures")
+
+    // 计算idf值，并根据向量空间模型中的tf值获得tfidf
+    val idfModel = new IDF()
+      .setInputCol(cvModel.getOutputCol)
+      .setOutputCol("features")
+      .setMinDocFreq(0)
+
+    val idfModel2 = new IDF()
+      .setInputCol(cvModel.getOutputCol)
+      .setOutputCol("features")
+      .setMinDocFreq(2)
+
+    //  val inppput = new ObjectInputStream(new FileInputStream("D:/idfModel"))
+    //  val idfModel = inppput.readObject().asInstanceOf[IDF]
+
+    val featureSelector = new ChiSqSelector()
+      .setNumTopFeatures(500)
+      .setFeaturesCol(idfModel.getOutputCol)
+      .setLabelCol("label")
+      .setOutputCol("selectedFeatures")
+
+    val vectorSpacePipline = new Pipeline()
+      .setStages(Array(stopWordsRemover, cvModel, idfModel, featureSelector))
+    val vectorSpacePiplineM = vectorSpacePipline.fit(dataDF)
+    val trainCM = vectorSpacePiplineM.transform(dataDF)
+    val testCM = vectorSpacePiplineM.transform(testDF)
+
+    val vectorSpacePipline2 = new Pipeline()
+      .setStages(Array(stopWordsRemover, cvModel, idfModel2, featureSelector))
+    val vectorSpacePiplineM2 = vectorSpacePipline2.fit(dataDF)
+    val trainCM2 = vectorSpacePiplineM2.transform(dataDF)
+    trainCM.show
+    trainCM2.show
+
+    // 转换数据类型
+    val trainData = trainCM.select("label", "selectedFeatures").map(line => {
+      LabeledPoint(line.getDouble(0), line.getAs[SparseVector](1))
+    })
+
+    val testData = testCM.select("label", "selectedFeatures").map(line => {
+      LabeledPoint(line.getDouble(0), line.getAs[SparseVector](1))
+    })
+
+    val nbModel = NaiveBayes.train(trainData, 1.0, "multinomial")
+
+    val predictionAndLabels = testData.map {line =>
+      val prediction = nbModel.predict(line.features)
+      (prediction, line.label)
+    }
+
+    val metrics = new MulticlassMetrics(predictionAndLabels)
+    println("Confusion matrix:")
+    println(metrics.confusionMatrix)
+
+    // Precision by label
+    val labels = metrics.labels
+    labels.foreach { l =>
+      println(s"Precision($l) = " + metrics.precision(l))
+    }
+
+    // Recall by label
+    labels.foreach { l =>
+      println(s"Recall($l) = " + metrics.recall(l))
+    }
+
+    // False positive rate by label
+    labels.foreach { l =>
+      println(s"FPR($l) = " + metrics.falsePositiveRate(l))
+    }
+
+    // F-measure by label
+    labels.foreach { l =>
+      println(s"F1-Score($l) = " + metrics.fMeasure(l))
+    }
+  }
+
+
+
   val conf = new SparkConf().setAppName("mltest").setMaster("local")
   val sc = new SparkContext(conf)
+
 
   val data = Source.fromFile("D:\\WorkSpace\\Spark_WorkSpace\\ein" +
     "\\text_classification\\1.1\\Spark_NLP_suit\\src\\main" +
@@ -113,7 +232,8 @@ private object TrainingProcess extends App{
 
   val Array(trainDataRDD, testDataRDD) = dataRDD.randomSplit(Array(0.7, 0.3))
 
-  trainingProcessWithRDD(trainDataRDD, testDataRDD)
+//  trainingProcessWithRDD(trainDataRDD, testDataRDD)
+  trainingProcessWithDF(sc, trainDataRDD, testDataRDD)
 
 
 
