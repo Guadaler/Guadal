717be095d48d423b643ca41241b1f5fed0ebf8b9
STHSF <woailiyu@163.com>
2016-03-21 14:49:09 +0800

    commit code again

--- text_classification/1.1/Spark_NLP_suit/src/main/scala/com/kunyandata/nlpsuit/util/textProcess.scala
+++ text_classification/1.1/Spark_NLP_suit/src/main/scala/com/kunyandata/nlpsuit/util/textProcess.scala
@@ -1,107 +1,36 @@
 package com.kunyandata.nlpsuit.util
 
-import java.io.StringWriter
-import java.util
+import org.apache.spark.{SparkContext, SparkConf}
 
-import org.apache.commons.io.IOUtils
-import org.apache.http.NameValuePair
-import org.apache.http.client.entity.UrlEncodedFormEntity
-import org.apache.http.client.methods.HttpPost
-import org.apache.http.impl.client.HttpClients
-import org.apache.http.message.BasicNameValuePair
+import scala.io.Source
 
 /**
   * Created by QQ on 2016/3/18.
   */
-class textProcess {
-   @native def write2proc(sentence: String): String
-}
 
+object textProcess extends App{
 
-object textProcess{
-  /**
-    * 通过网络接口获得分词结果
-    *
-    * @param content 需要分词的字符串
-    * @return json格式的分词结果
-    */
-  private def sendPost(content: String): String = {
+  case class RawDataRecord(ID: String, category: String ,labels: Double ,text: String)
 
-    val url = "http://112.124.49.59/cgi-bin/nlp/segment/v1/segment_word.fcgi"
-    val httpclient = HttpClients.createDefault()
-    val httpPost = new HttpPost(url)
+  val conf = new SparkConf().setMaster("local").setAppName("textProcess")
+  val sc = new SparkContext(conf)
 
-    // Request parameters and other properties.
-    val params = new util.ArrayList[NameValuePair]()
+  //导入预处理String数据
+  val srcRDD = sc.textFile("/users/li/Intellij/Native-Byes/nativebyes/2.txt").map {
+    line =>
+      val data = line.split(",")
+      RawDataRecord(data(0),data(1),labels = if(data(1) == "881155" ) 1.0 else 0.0, data(2))
+  }.toString()
 
-    params.add(new BasicNameValuePair("uid", "100001"))
-    params.add(new BasicNameValuePair("token", "qR3E1122SDD8B31EFBBD"))
-    params.add(new BasicNameValuePair("content", content))
-    httpPost.setEntity(new UrlEncodedFormEntity(params, "UTF-8"))
-    httpPost.setHeader("Content-Type", "application/x-www-form-urlencoded")
+  //导入本地停用词
+  var stopWord = Source.fromFile("/users/li/Intellij/Native-Byes/nativebyes/stop_words_CN" )
+  var stopWords = stopWord.getLines().toArray
 
+  // 调入分词系统
+  val context = WordSeg.splitWord(srcRDD,0).toCharArray
 
-    var result = ""
-    //Execute and get the response.
-    val response = httpclient.execute(httpPost)
-    val entity = response.getEntity
-
-    if (entity != null) {
-      val inputStream = entity.getContent
-      try {
-        val writer = new StringWriter()
-        IOUtils.copy(inputStream, writer, "utf-8")
-        result = writer.toString
-      } catch {
-        case e: Exception =>
-          e.printStackTrace()
-          result = "{\"status\":0}"
-      } finally {
-        inputStream.close()
-      }
-      result
-    } else {
-      "{\"status\":0}"
-    }
-  }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-  def splitword(content: String, source: Int): String = {
-
-    source match {
-      case 0 =>
-        val textProcess = new textProcess
-        try {
-          System.loadLibrary("WordSeg")
-          toJson(textProcess.write2proc(content))
-        } catch {
-          case e: UnsatisfiedLinkError =>
-            "Cannot load com.kunyandata.nlpsuit.util.WordSeg library:\n " + e.toString
-        }
-
-      case 1 =>
-
-    }
-  }
+  val result = WordSeg.removeStopWords(context,stopWords)
+  sc.stop()
 
 
 }
