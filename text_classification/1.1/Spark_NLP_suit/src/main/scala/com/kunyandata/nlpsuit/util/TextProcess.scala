649b4d5dfef74feabfa94fe2540c59864558a9cc
STHSF <woailiyu@163.com>
2016-03-23 18:19:07 +0800

    commit code

--- text_classification/1.1/Spark_NLP_suit/src/main/scala/com/kunyandata/nlpsuit/util/TextProcess.scala
+++ text_classification/1.1/Spark_NLP_suit/src/main/scala/com/kunyandata/nlpsuit/util/TextProcess.scala
@@ -4,7 +4,6 @@ import java.io.{BufferedWriter, File, FileWriter}
 
 import org.apache.spark.broadcast.Broadcast
 import org.apache.spark.{SparkConf, SparkContext}
-
 import scala.collection.mutable.ArrayBuffer
 import scala.io.Source
 
@@ -19,14 +18,15 @@ object TextProcess {
   /**
     * 调用WordSeq里面的函数实现字符串的分词和去停,并分装成方法
     *
-    * @param context     需要处理的字符串
+    * @param content 需要处理的字符串
     * @param stopWordsBr 停用词
     * @return 返回分词去停后的结果
     */
-  def process(context: String, stopWordsBr: Broadcast[Array[String]]): Array[String] = {
+  def process(content: String, stopWordsBr: Broadcast[Array[String]]): Array[String] = {
 
+    val contentString = content.replaceAll("[\n\t\r\f\b ]", ",")
     //实现分词
-    val splitWords = WordSeg.splitWord(context, 1)
+    val splitWords = WordSeg.splitWord(contentString, 1)
 
     //读取分词内容并转化成Array格式
     val stopWord = WordSeg.getWords(splitWords)
@@ -41,7 +41,7 @@ object TextProcess {
 
     val conf = new SparkConf().setAppName("mltest").setMaster("local")
     val sc = new SparkContext(conf)
-    val stopWords = Source.fromFile("/users/li/Intellij/Native-Byes/nativebyes/stop_words_CN").getLines().toArray
+    val stopWords = Source.fromFile("/Users/li/kunyan/nlp/text_classification/1.1/Spark_NLP_suit/src/main/resources/dicts/stop_words_CN").getLines().toArray
     val stopWordsBr = sc.broadcast(stopWords)
 
     // 驱动名称
@@ -58,9 +58,10 @@ object TextProcess {
     val password = "root"
     // 读取sql里面的内容
     // val sqlString="SELECT * FROM indus_text_with_label WHERE id>3000 and id<=15000"
-    val sqlString = "SELECT url, content FROM stock.indus_text_with_label group by url"
-    // 调用GetResultSet函数获取数据库中的数据
-    val result = MySQLUtil.GetResultSet(driver, jdbcUrl, username, password, sqlString)
+    val sqlString = "SELECT url, content FROM stock.indus_text_with_label group by url limit 10"
+    // 调用函数获取数据库中的数据
+    val connection = MySQLUtil.getConnect(driver, jdbcUrl, username, password)
+    val result = MySQLUtil.getResult(connection,sqlString)
 
     // 解析数据库中的数据
     //    var results = new ArrayBuffer[Tuple4[String,String,String,String]]
@@ -72,31 +73,28 @@ object TextProcess {
     //      results.append((id, indus_code, title, content, url)
     //    }
 
-
     // 定义splitResults 保存分词结果
     val splitResults = new ArrayBuffer[(String, Array[String])]
     while (result.next()) {
       val url = result.getString("url").trim
       val content = result.getString("content").trim
+      // println(content)
       val wordsList = TextProcess.process(content, stopWordsBr)
+      // println(wordsList.toSeq)
       splitResults.append((url, wordsList))
     }
 
-
-
-    // 读取的数据保存到本地txt文件
-    //    print(splitResults)
-
+    // 断开连接
+    connection.close()
+    // 读取的数据写入到本地txt文件
     val splitResult = splitResults.toArray
     // 读取的数据保存到本地txt文件
     val DataFile = new File("out.txt")
-    val bw = new BufferedWriter(new FileWriter(DataFile))
+    val bufferWriter = new BufferedWriter(new FileWriter(DataFile))
     splitResult.foreach(x => {
-      bw.write(x._1 + "," + x._2 + "\n")
+      bufferWriter.write(x._1 + "," + x._2.mkString(" ") + "\n")
     })
-    bw.flush()
-    bw.close()
-
-
+    bufferWriter.flush()
+    bufferWriter.close()
   }
 }
