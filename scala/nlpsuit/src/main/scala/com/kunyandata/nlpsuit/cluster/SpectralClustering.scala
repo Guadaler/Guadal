07127c51ccbdc52b444d2c601d9a380b99146abf
QQ <ricklovelisa@126.com>
2016-05-08 22:57:43 +0800

    update code

--- scala/nlpsuit/src/main/scala/com/kunyandata/nlpsuit/cluster/SpectralClustering.scala
+++ scala/nlpsuit/src/main/scala/com/kunyandata/nlpsuit/cluster/SpectralClustering.scala
@@ -4,9 +4,10 @@ package com.kunyandata.nlpsuit.cluster
   * Created by QQ on 5/6/16.
   */
 
-import breeze.linalg
-import breeze.linalg.{DenseMatrix, DenseVector}
+import breeze.linalg._
 import breeze.linalg.eig.Eig
+import com.kunyandata.nlpsuit.Statistic.Similarity
+import org.apache.spark.rdd.RDD
 import org.apache.spark.{SparkConf, SparkContext}
 import org.apache.spark.mllib.clustering.KMeans
 import org.apache.spark.mllib.stat.Statistics
@@ -15,44 +16,88 @@ import scala.io.Source
 
 object SpectralClustering {
 
+  def countAndIndex(content:Array[String], wordList: Array[String]): Array[(Int, Double)] = {
+    val result = wordList.map(word => {
+      if (content.contains(word)) (wordList.indexOf(word), content.count(_ == word).toDouble)
+      else (wordList.indexOf(word), 0.0)
+    })
+    result
+  }
+
+  // 计算相关矩阵
+  def createCorrMatrix(dataRDD: RDD[String]): DenseMatrix[Double] = {
+
+    null
+  }
+
   def main(args: Array[String]) {
 
     val conf = new SparkConf()
       .setAppName("SClusterTest")
       .setMaster("local")
-      .set("spark.local.ip", "192.168.2.65")
-      .set("spark.driver.host", "192.168.2.90")
+//      .set("spark.local.ip", "192.168.2.65")
+//      .set("spark.driver.host", "192.168.2.90")
 
     val sc = new SparkContext(conf)
 
+//    ++++++++++++++++++++++++++++++++++++++ 计算 adjacency matrix ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
     //获取数据
-    val data = sc.parallelize(Source
-      .fromFile("/home/QQ/Documents/trainingWithIndus/仪电仪表")
-      .getLines()
-      .toSeq).map(line => {
-      val Array(label, content) = line.split("\t")
-      (label, content.split(","))
-    })
+//    var id = 0
+//    val data = sc.parallelize(Source
+//      .fromFile("D:/mlearning/trainingData/trainingWithIndus/仪电仪表")
+//      .getLines().toSeq)
+//      .map(line => {
+//      val temp = line.split("\t")
+//      val result = (id, temp(1).split(","))
+//      id += 1
+//      result
+//    }).cache()
 
-    // 获得词序列
+    val data = sc.parallelize(Seq((0, Array("a", "b", "c", "d")), (1, Array("a", "c", "d", "e")), (2, Array("b", "d", "f", "g", "k")))).cache()
+
+    // 获得该新闻数据的词典
     val wordsList = data.map(_._2).flatMap(_.toList).collect().distinct.sorted
-    println(wordsList.length)
-    val temp = DenseVector.zeros[Double](wordsList.length)
+    val wordsListBr = sc.broadcast(wordsList)
+    val docTermRowNum = data.count().toInt
+    val docTermColNum = wordsList.length
+    val docTermMatrixBr = sc.broadcast(DenseMatrix.zeros[Double](docTermRowNum, docTermColNum))
 
-    val docTermMatrix = data.map(line => {
-      wordsList.foreach(word => {
-        if(line._2.contains(word)) {
-          temp(wordsList.indexOf(word)) = wordsList.count(_ == word)
+    data.foreach(line => {
+      wordsListBr.value.foreach(word => {
+        if (line._2.contains(word)) {
+          docTermMatrixBr.value(line._1, wordsListBr.value.indexOf(word)) = line._2.count(_ == word)
         }
       })
-      temp
-    }).collect()
-    val a = DenseMatrix(docTermMatrix)
-    val b = DenseMatrix(Array(1.0,2.0), Array(3.0,4.0))
+    })
+
+    println(docTermMatrixBr.value)
+
+    val corrMatrixBr = sc.broadcast(DenseMatrix.zeros[Double](docTermColNum, docTermColNum))
+
+    sc.parallelize(wordsListBr.value.indices).foreach(index => {
+      val wordVector = docTermMatrixBr.value(::, index)
+      val cosineDistanceArray = wordsListBr.value.indices.map(tempIndex => {
+        val tempVector = docTermMatrixBr.value(::, tempIndex)
+        Similarity.cosineDistance(wordVector, tempVector)
+      }).toArray
+      val cosineDistanceVector = DenseVector(cosineDistanceArray)
+      corrMatrixBr.value(index, ::) := cosineDistanceVector.t
+    })
+    println(corrMatrixBr.value)
+//    ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+//    ++++++++++++++++++++++++++++++++++++++++++ 计算degree matrix & laplacian matrix ++++++++++++++++++++++++++++++++++++++++++++++
+//    L = D - A
+    val degreeMatrix = diag(sum(corrMatrixBr.value(*, ::)))
+    val laplacianMatrix = degreeMatrix :- corrMatrixBr.value
+    val Eig(a, _, c) = eig(laplacianMatrix)
+    println(a.activeIterator.toArray.toSeq)
+
+    println(c.activeIterator.toArray.toSeq)
 
 
-    println(a)
-    println(b)
+//    Statistics.
+//    println(docTermMatrix(1, 2715))
 
 
   }
